95ed864c4e3e16c3cdcba04d7429950fc67bda00
PedrexDev <pedrex@post.cz>
2023-01-12 19:52:43 +0100

    removed some orientation things in the code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
-const https = require('https')
-const bodyParser = require("body-parser");
-const fs = require('fs');
 const app = express();
 const port = 80;
 
@@ -14,32 +10,23 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-////////////////////////////////////////////////
-// BASE ENDPOINTS
-////////////////////////////////////////////////
 app.get('/', (req, res) => {
   res.render('index');
-  /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/terms', (req, res) => {
   res.render('terms');
-  /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/privacy-policy', (req, res) => {
   res.render('privacy');
-  /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/team', (req, res) => {
   res.render('team');
-  /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
-////////////////////////////////////////////////
-// ERROR PAGES
-////////////////////////////////////////////////
+
 app.use(function(req, res, next) {
   res.status(404);
 
