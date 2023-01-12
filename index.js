f3e155c094a3350dbfc0e510d32357984fd7a231
PedrexDev <pedrex@post.cz>
2023-01-12 19:41:36 +0100

    getting it to be ready

--- index.js
+++ index.js
@@ -18,25 +18,24 @@ app.use(express.urlencoded({
 // BASE ENDPOINTS
 ////////////////////////////////////////////////
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
   /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/terms', (req, res) => {
-  res.render('terms')
+  res.render('terms');
   /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/privacy-policy', (req, res) => {
-  res.render('privacy')
+  res.render('privacy');
   /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
 
 app.get('/team', (req, res) => {
-  res.render('team')
+  res.render('team');
   /*res.sendFile(path.join(__dirname, '/html/index.html'));*/
 });
-
 
 ////////////////////////////////////////////////
 // ERROR PAGES
@@ -44,15 +43,13 @@ app.get('/team', (req, res) => {
 app.use(function(req, res, next) {
   res.status(404);
 
-  res.sendFile(path.join(__dirname, '/html/errors/404.html'));
-
+  res.render('404');
 });
 
 app.use(function(req, res, next) {
-    res.status(403);
+  res.status(403);
   
-    res.sendFile(path.join(__dirname, '/html/errors/403.html'));
-  
+  res.render('403')
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}.`))
